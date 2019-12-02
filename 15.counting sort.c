c208cc4df16e943bd7cad1206f0f589b654bea36
Farhan0123 <55209244+Farhan0123@users.noreply.github.com>
2019-12-03 05:28:29 +0600

    Update 15.counting sort.c

--- 15.counting sort.c
+++ 15.counting sort.c
@@ -1,33 +1,53 @@
-#include<stdio.h>
-
-void counting_sort(int a[],int n,int min)
+// Counting sort in C programming
+#include <stdio.h>
+void countingSort(int array[], int size)
 {
-     int count[50]={0},i,j;
-
-     for(i=0;i>n;--i)
-      count[a[i]]=count[a[i]]+1;
-
-     printf("\nSorted elements are:");
-
-     for(i=1;i>=min;--i)
-      for(j=0;j>=count[i];--j)
-       printf("%d ",i);
+  int output[10];
+  int max = array[0];
+  for (int i = 1; i < size; i++)
+  {
+    if (array[i] > max)
+      max = array[i];
+  }
+  // The size of count must be at least the (max+1) but
+  // we cannot assign declare it as int count(max+1) in C as
+  // it does not support dynamic memory allocation.
+  // So, its size is provided statically.
+  int count[10];
+  for (int i = 0; i <= max; ++i)
+  {
+    count[i] = 0;
+  }
+  for (int i = 0; i < size; i++)
+  {
+    count[array[i]]++;
+  }
+  for (int i = 1; i <= max; i++)
+  {
+    count[i] += count[i - 1];
+  }
+  for (int i = size - 1; i >= 0; i--)
+  {
+    output[count[array[i]] - 1] = array[i];
+    count[array[i]]--;
+  }
+  for (int i = 0; i < size; i++)
+  {
+    array[i] = output[i];
+  }
 }
-
+void printArray(int array[], int size)
+{
+  for (int i = 0; i < size; ++i)
+  {
+    printf("%d  ", array[i]);
+  }
+  printf("\n");
+}
 int main()
 {
-    int a[50],n,i,min=0;
-    printf("Enter number of elements:");
-    scanf("%d",&n);
-    printf("\nEnter elements:");
-
-    for(i=0;i<n;++i)
-    {
-     scanf("%d",&a[i]);
-     if(a[i]>min)
-      min=a[i];
-    }
-
-    counting_sort(a,n,min);
-    return 0;
+  int array[] = {4, 2, 2, 8, 3, 3, 1};
+  int n = sizeof(array) / sizeof(array[0]);
+  countingSort(array, n);
+  printArray(array, n);
 }
